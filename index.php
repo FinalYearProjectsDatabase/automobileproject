1e45360e8ad64685c592aff2a47f063c71323a96
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:53 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php 
+    require_once('config/index-settings.php');
+    require(__DIR__.'/components/index-head.php')
+?>
+
+<body class="rev-10-body">
+    <!-- preloader -->
+    <?php require(__DIR__.'/components/index-preloader.php')?>
+
+    <!--------------------------------- PRODUCT QUICK VIEW PANEL START --------------------------------->
+    <?php require(__DIR__.'/components/index-product-quick-view.php')?>
+    <!--------------------------------- PRODUCT QUICK VIEW PANEL END --------------------------------->
+
+
+    <!--------------------------------- HEADER CART LIST START --------------------------------->
+    <?php require(__DIR__.'/components/index-header-cart.php')?>
+    <!--------------------------------- HEADER CART LIST END --------------------------------->
+
+
+    <!--------------------------------- HEADER WISH LIST START --------------------------------->
+    <?php require(__DIR__.'/components/index-header-wish.php')?>
+    <!--------------------------------- HEADER WISH LIST END --------------------------------->
+
+
+    <!--------------------------------- MOBILE MENU START --------------------------------->
+    <?php require(__DIR__.'/components/index-mobile-menu.php')?>
+    <!--------------------------------- MOBILE MENU END --------------------------------->
+
+
+    <!--------------------------------- HEADER SECTION START --------------------------------->
+    <?php require(__DIR__.'/components/index-header-section.php')?>
+    <!--------------------------------- HEADER SECTION END --------------------------------->
+
+
+    <!--------------------------------- BANNER SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-banner-section.php')?>
+    <!--------------------------------- BANNER SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- CATEGORIES SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-category-section.php')?>
+    <!--------------------------------- CATEGORIES SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- FLASH SALE SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-flash-sale.php')?>
+    <!--------------------------------- FLASH SALE SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- VIDEO SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-video-section.php')?>
+    <!--------------------------------- VIDEO SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- BEST SELLING SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-best-selling.php')?>
+    <!--------------------------------- BEST SELLING SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- AD SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-ad-one.php')?>
+    <!--------------------------------- AD SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- CAR PARTS SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-car-parts-section.php')?>
+    <!--------------------------------- CAR PARTS SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- AD SECTION 2 STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-ad-two.php')?>
+    <!--------------------------------- AD SECTION 2 ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- FEATURES SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-features-section.php')?>
+    <!--------------------------------- FEATURES SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- AD SECTION 3 STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-ad-three.php')?>
+    <!--------------------------------- AD SECTION 3 SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- TESTIMONIAL SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-testimonials-section.php')?>
+    <!--------------------------------- TESTIMONIAL SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- BLOG SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-blog.php')?>
+    <!--------------------------------- BLOG SECTION ENDS HERE --------------------------------->
+
+
+    <!--------------------------------- FOOTER SECTION STARTS HERE --------------------------------->
+    <?php require(__DIR__.'/components/index-footer-version.php')?>
+    <!--------------------------------- FOOTER SECTION ENDS HERE --------------------------------->
+
+<?php require(__DIR__.'/components/index-footer.php')?>
