0c89f18bdf66938490a29e2470fdc300e2a8d282
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:32 +0000

    Create index-flash-sale.php

---
+++ components/index-flash-sale.php
@@ -0,0 +1,126 @@
+<section class="flash-sale rev-14-flash-sale">
+        <div class="container">
+            <div class="rev-14-section-heading">
+                <h2 class="rev-14-section-title">flash sale</h2>
+                <a href="#" class="rev-14-def-btn">Show More</a>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-lg-3 col-md-4 col-6 col-xs-12">
+                    <div class="single-product-card rev-11-single-product rev-14-single-product">
+                        <div class="single-product__img part-img">
+                            <img src="assets/images/rev-14-prod-1.png" alt="Product Image">
+                            <div class="single-product__actions">
+                                <button class="compare-btn"><i class="fa-regular fa-shuffle"></i></button>
+                                <button class="add-to-wishlist-btn"><i class="fa-regular fa-heart"></i></button>
+                                <button class="add-to-cart-btn"><i class="fa-regular fa-shopping-bag"></i></button>
+                                <button class="quick-view"><i class="fa-regular fa-eye"></i></button>
+                            </div>
+                        </div>
+
+                        <div class="single-product__txt">
+                            <div class="row">
+                                <div class="col-xl-8">
+                                    <h4 class="single-product__title">Go Pro Ultra 4K High Polo Modern Camera</h4>
+                                </div>
+
+                                <div class="col-xl-4">
+                                    <div class="single-product__price">
+                                        <span class="current-price">$144.95</span>
+                                        <span class="prev-price text-decoration-line-through">$200.00</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6 col-xs-12">
+                    <div class="single-product-card rev-11-single-product rev-14-single-product">
+                        <div class="single-product__img part-img">
+                            <img src="assets/images/rev-14-prod-2.png" alt="Product Image">
+                            <div class="single-product__actions">
+                                <button class="compare-btn"><i class="fa-regular fa-shuffle"></i></button>
+                                <button class="add-to-wishlist-btn"><i class="fa-regular fa-heart"></i></button>
+                                <button class="add-to-cart-btn"><i class="fa-regular fa-shopping-bag"></i></button>
+                                <button class="quick-view"><i class="fa-regular fa-eye"></i></button>
+                            </div>
+                        </div>
+
+                        <div class="single-product__txt">
+                            <div class="row">
+                                <div class="col-xl-8">
+                                    <h4 class="single-product__title">Go Pro Ultra 4K High Polo Modern Camera</h4>
+                                </div>
+
+                                <div class="col-xl-4">
+                                    <div class="single-product__price">
+                                        <span class="current-price">$144.95</span>
+                                        <span class="prev-price text-decoration-line-through">$200.00</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6 col-xs-12">
+                    <div class="single-product-card rev-11-single-product rev-14-single-product">
+                        <div class="single-product__img part-img">
+                            <img src="assets/images/rev-14-prod-3.png" alt="Product Image">
+                            <div class="single-product__actions">
+                                <button class="compare-btn"><i class="fa-regular fa-shuffle"></i></button>
+                                <button class="add-to-wishlist-btn"><i class="fa-regular fa-heart"></i></button>
+                                <button class="add-to-cart-btn"><i class="fa-regular fa-shopping-bag"></i></button>
+                                <button class="quick-view"><i class="fa-regular fa-eye"></i></button>
+                            </div>
+                        </div>
+
+                        <div class="single-product__txt">
+                            <div class="row">
+                                <div class="col-xl-8">
+                                    <h4 class="single-product__title">Go Pro Ultra 4K High Polo Modern Camera</h4>
+                                </div>
+
+                                <div class="col-xl-4">
+                                    <div class="single-product__price">
+                                        <span class="current-price">$144.95</span>
+                                        <span class="prev-price text-decoration-line-through">$200.00</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6 col-xs-12">
+                    <div class="single-product-card rev-11-single-product rev-14-single-product">
+                        <div class="single-product__img part-img">
+                            <img src="assets/images/rev-14-prod-4.png" alt="Product Image">
+                            <div class="single-product__actions">
+                                <button class="compare-btn"><i class="fa-regular fa-shuffle"></i></button>
+                                <button class="add-to-wishlist-btn"><i class="fa-regular fa-heart"></i></button>
+                                <button class="add-to-cart-btn"><i class="fa-regular fa-shopping-bag"></i></button>
+                                <button class="quick-view"><i class="fa-regular fa-eye"></i></button>
+                            </div>
+                        </div>
+
+                        <div class="single-product__txt">
+                            <div class="row">
+                                <div class="col-xl-8">
+                                    <h4 class="single-product__title">Go Pro Ultra 4K High Polo Modern Camera</h4>
+                                </div>
+
+                                <div class="col-xl-4">
+                                    <div class="single-product__price">
+                                        <span class="current-price">$144.95</span>
+                                        <span class="prev-price text-decoration-line-through">$200.00</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
