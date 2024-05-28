cf834f1efdc882046e763be46b1346f6d0c62fd9
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:33 +0000

    Create index-footer-version.php

---
+++ components/index-footer-version.php
@@ -0,0 +1,95 @@
+<footer class="footer rev-10-footer rev-14-footer">
+        <div class="main-footer">
+            <div class="container">
+                <div class="custom-row">
+                    <div class="custom-col-2">
+                        <div class="footer-about">
+                            <div class="footer-logo">
+                                <img src="assets/images/logos/logo-5.png" alt="Logo">
+                            </div>
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+                            <ul class="d-flex align-items-center">
+                                <li>
+                                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                                </li>
+
+                                <li><a href="#"><i class="fa-brands fa-pinterest-p"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class=" custom-col-2">
+                        <div class="link-wrap">
+                            <div class="footer-link">
+                                <h3 class="footer-title">quick links</h3>
+                                <ul>
+                                    <li><a href="shop.html">All Products</a></li>
+                                    <li><a href="#">Payment Method</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                    <li><a href="#">Faq & Support</a></li>
+                                    <li><a href="blog-details.html">Blog Post</a></li>
+                                </ul>
+                            </div>
+                            <div class="footer-link">
+                                <h3 class="footer-title">shop info</h3>
+                                <ul>
+                                    <li><a href="#">All Categories</a></li>
+                                    <li><a href="#">About Shop</a></li>
+                                    <li><a href="#">New Products</a></li>
+                                    <li><a href="#">Discount Offer</a></li>
+                                    <li><a href="#">Summer Sale</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="custom-col">
+                        <div class="footer-link">
+                            <h3 class="footer-title">account info</h3>
+                            <ul>
+                                <li><a href="register.html">Sign In</a></li>
+                                <li><a href="#">Shipping Address</a></li>
+                                <li><a href="wishlist.html">Wishlist</a></li>
+                                <li><a href="cart.html">Order History</a></li>
+                                <li><a href="account.html">My Account</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="custom-col">
+                        <div class="footer-subscribe">
+                            <h3 class="footer-title">contact us</h3>
+                            <h5>Subscribe to our newsletter</h5>
+                            <form>
+                                <input type="email" name="email" placeholder="Your Email">
+                                <button type="submit"><i class="fa-solid fa-paper-plane"></i></button>
+                            </form>
+
+                            <ul class="footer-info">
+                                <li>Address: Your Address, 1234, Country</li>
+                                <li><a href="mailto:test@example.com">Email: test@example.com</a></li>
+                                <li><a href="tel:+0123-456-789">Phone: +0123-456-789</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <p>&copy; Copyright Revel All Right Reserved</p>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="part-img d-flex justify-content-md-end justify-content-center">
+                            <img src="assets/images/payment-gateway.png" alt="Image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
