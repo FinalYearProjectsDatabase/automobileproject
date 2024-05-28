39374bc7e33fe772fa8f224d6b4ffa4cae95154c
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:20 +0000

    Create index-ad-two.php

---
+++ components/index-ad-two.php
@@ -0,0 +1,21 @@
+<section class="ad-section">
+        <div class="container">
+            <div class="row justify-content-center g-4">
+                <div class="col-md-4 col-6 col-xs-12">
+                    <a href="#">
+                        <img src="assets/images/rev-14-ad-banner-3.png" alt="Ad Banner">
+                    </a>
+                </div>
+                <div class="col-md-4 col-6 col-xs-12">
+                    <a href="#">
+                        <img src="assets/images/rev-14-ad-banner-4.png" alt="Ad Banner">
+                    </a>
+                </div>
+                <div class="col-md-4 col-6 col-xs-12">
+                    <a href="#">
+                        <img src="assets/images/rev-14-ad-banner-5.png" alt="Ad Banner">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
