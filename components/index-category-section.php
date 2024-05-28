4a476c382fab1a37aa7e75521a26eaa2981a16ee
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:29 +0000

    Create index-category-section.php

---
+++ components/index-category-section.php
@@ -0,0 +1,50 @@
+<section class="categories-section rev-14-categories">
+        <div class="container">
+            <div class="row gy-4">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="d-flex flex-column first-col">
+                        <div class="single-category">
+                            <div class="single-category__txt">
+                                <h5>High Pro Sports Car</h5>
+                                <a href="#" class="rev-14-def-btn">buy now</a>
+                            </div>
+                        </div>
+
+                        <div class="single-category single-category-2">
+                            <div class="single-category__txt">
+                                <h5>Scania New Truck</h5>
+                                <a href="#" class="rev-14-def-btn">buy now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 order-lg-2 order-3">
+                    <div class="single-category single-category-3">
+                        <div class="single-category__txt">
+                            <h5>Top Lamborghini Veneno Car</h5>
+                            <a href="#" class="rev-14-def-btn">buy now</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-sm-6 order-lg-3 order-2">
+                    <div class="d-flex flex-column third-col">
+                        <div class="single-category single-category-4">
+                            <div class="single-category__txt">
+                                <h5>BMW Ultra Motorcycle</h5>
+                                <a href="#" class="rev-14-def-btn">buy now</a>
+                            </div>
+                        </div>
+
+                        <div class="single-category single-category-5">
+                            <div class="single-category__txt">
+                                <h5>Audi High Performence Car</h5>
+                                <a href="#" class="rev-14-def-btn">buy now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
