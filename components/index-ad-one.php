da255d680cb738637bf367707b383c3486dbc855
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:19 +0000

    Create index-ad-one.php

---
+++ components/index-ad-one.php
@@ -0,0 +1,16 @@
+<section class="ad-section">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-lg-6">
+                    <a href="#">
+                        <img src="assets/images/rev-14-ad-banner-1.png" alt="Ad Banner">
+                    </a>
+                </div>
+                <div class="col-lg-6">
+                    <a href="#">
+                        <img src="assets/images/rev-14-ad-banner-2.png" alt="Ad Banner">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
