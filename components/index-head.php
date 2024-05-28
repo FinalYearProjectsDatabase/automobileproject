e21d576642d3232f03161ea4386b15f79082bc02
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:36 +0000

    Create index-head.php

---
+++ components/index-head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revel eCommerce-Multi vendor Ecommerce HTML Template</title>
+
+    <link rel="icon" type="image/x-icon" href="favicon.png">
+    <link rel="stylesheet" href="assets/vendor/css/all.min.css">
+    <link rel="stylesheet" href="assets/vendor/flaticon/flaticon.css">
+    <link rel="stylesheet" href="assets/vendor/css/nice-select.css">
+    <link rel="stylesheet" href="assets/vendor/css/flags.css">
+    <link rel="stylesheet" href="assets/vendor/css/slick.css">
+    <link rel="stylesheet" href="assets/vendor/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendor/css/meanmenu.css">
+    <link rel="stylesheet" href="assets/vendor/css/modal-video.min.css">
+    <link rel="stylesheet" href="assets/vendor/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/vendor/css/sharp-regular.min.css">
+    <link rel="stylesheet" href="assets/vendor/css/sharp-solid.min.css">
+    <link rel="stylesheet" href="assets/vendor/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
