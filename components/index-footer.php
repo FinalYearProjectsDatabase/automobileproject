8bfb8924159f0450955b7ef2a2c6b69b533d7e41
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:35 +0000

    Create index-footer.php

---
+++ components/index-footer.php
@@ -0,0 +1,13 @@
+<script src="assets/vendor/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/vendor/js/jquery.nice-select.min.js"></script>
+    <script src="assets/vendor/js/jquery.flagstrap.min.js"></script>
+    <script src="assets/vendor/js/slick.min.js"></script>
+    <script src="assets/vendor/js/owl.carousel.min.js"></script>
+    <script src="assets/vendor/js/jquery.meanmenu.min.js"></script>
+    <script src="assets/vendor/js/jquery.syotimer.min.js"></script>
+    <script src="assets/vendor/js/jquery-modal-video.min.js"></script>
+    <script src="assets/vendor/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
