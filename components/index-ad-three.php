6c17da831d1398e93ece52c86f3147e2a5a3c8da
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:17 +0000

    Create index-ad-three.php

---
+++ components/index-ad-three.php
@@ -0,0 +1,7 @@
+<section class="ad-section pb-80 pb-md-30">
+        <div class="container">
+            <a href="#">
+                <img src="assets/images/rev-14-ad-banner-6.png" alt="Ad Banner">
+            </a>
+        </div>
+    </section>
