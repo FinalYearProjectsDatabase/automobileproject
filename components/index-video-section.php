9a3f55c26a38339c697b341c8f3faedf07eeb379
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:47 +0000

    Create index-video-section.php

---
+++ components/index-video-section.php
@@ -0,0 +1,6 @@
+<section class="rev-14-video-modal-section">
+        <div class="container">
+            <img src="assets/images/rev-14-modal-video-bg.png" alt="Background Banner">
+            <button class="rev-14-video-modal-btn popup-video-open" data-video-id="L61p2uyiMSo"><i class="fa-solid fa-sharp fa-play"></i></button>
+        </div>
+    </section>
