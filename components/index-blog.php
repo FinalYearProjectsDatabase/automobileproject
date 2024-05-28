7d6494680344d8c431b125fb360feada0d766502
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:26 +0000

    Create index-blog.php

---
+++ components/index-blog.php
@@ -0,0 +1,85 @@
+<section class="blog-section rev-10-blog-section rev-14-blog">
+        <div class="container">
+            <div class="rev-14-section-heading">
+                <h2 class="rev-14-section-title">Latest Blog Post</h2>
+            </div>
+            <div class="row gy-4 justify-content-center">
+                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
+                    <div class="rev-10-single-blog">
+                        <div class="rev-10-single-blog__img">
+                            <img src="assets/images/rev-10-blog-1.png" alt="Blog Image">
+                        </div>
+
+                        <div class="rev-10-single-blog__txt">
+                            <div class="rev-10-single-blog__author-and-date">
+                                <span class="blog-author">James Hames</span>
+                                <span class="blog-date">22 Oct 2021</span>
+                            </div>
+                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
+                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
+                                mos sodales suscipit tortor ditaemcos condimentum
+                                lacus meleifend.Quisquemos sodales suscipit.</p>
+                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
+                    <div class="rev-10-single-blog">
+                        <div class="rev-10-single-blog__img">
+                            <img src="assets/images/rev-10-blog-2.png" alt="Blog Image">
+                        </div>
+
+                        <div class="rev-10-single-blog__txt">
+                            <div class="rev-10-single-blog__author-and-date">
+                                <span class="blog-author">James Hames</span>
+                                <span class="blog-date">22 Oct 2021</span>
+                            </div>
+                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
+                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
+                                mos sodales suscipit tortor ditaemcos condimentum
+                                lacus meleifend.Quisquemos sodales suscipit.</p>
+                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
+                    <div class="rev-10-single-blog">
+                        <div class="rev-10-single-blog__img">
+                            <img src="assets/images/rev-10-blog-3.png" alt="Blog Image">
+                        </div>
+
+                        <div class="rev-10-single-blog__txt">
+                            <div class="rev-10-single-blog__author-and-date">
+                                <span class="blog-author">James Hames</span>
+                                <span class="blog-date">22 Oct 2021</span>
+                            </div>
+                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
+                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
+                                mos sodales suscipit tortor ditaemcos condimentum
+                                lacus meleifend.Quisquemos sodales suscipit.</p>
+                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
+                    <div class="rev-10-single-blog">
+                        <div class="rev-10-single-blog__img">
+                            <img src="assets/images/rev-10-blog-4.png" alt="Blog Image">
+                        </div>
+
+                        <div class="rev-10-single-blog__txt">
+                            <div class="rev-10-single-blog__author-and-date">
+                                <span class="blog-author">James Hames</span>
+                                <span class="blog-date">22 Oct 2021</span>
+                            </div>
+                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
+                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
+                                mos sodales suscipit tortor ditaemcos condimentum
+                                lacus meleifend.Quisquemos sodales suscipit.</p>
+                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
