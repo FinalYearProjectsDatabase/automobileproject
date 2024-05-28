a8f41736555f5dc1f97d638a9efaa601be285e0b
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:22 +0000

    Create index-banner-section.php

---
+++ components/index-banner-section.php
@@ -0,0 +1,43 @@
+<section class="banner-section rev-14-banner">
+        <div class="rev-14-banner-slider-container owl-carousel">
+            <div class="rev-14-banner-single-slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-sm-8">
+                            <div class="banner-txt">
+                                <h6>We're Largest car Selling Company</h6>
+                                <h1>Buy Your Dream Car With Us</h1>
+                                <a href="#" class="rev-14-def-btn">select car <i class="fa-sharp fa-solid fa-car"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rev-14-banner-single-slide rev-14-banner-single-slide-2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-sm-8">
+                            <div class="banner-txt">
+                                <h6>We're Largest car Selling Company</h6>
+                                <h1>Buy Your Dream Car With Us</h1>
+                                <a href="#" class="rev-14-def-btn">select car <i class="fa-sharp fa-solid fa-car"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rev-14-banner-single-slide rev-14-banner-single-slide-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-sm-8">
+                            <div class="banner-txt">
+                                <h6>We're Largest car Selling Company</h6>
+                                <h1>Buy Your Dream Car With Us</h1>
+                                <a href="#" class="rev-14-def-btn">select car <i class="fa-sharp fa-solid fa-car"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
