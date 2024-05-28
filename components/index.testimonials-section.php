8d095f305c59fe3a54f40162c60d4a69ccbc5856
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-28 05:43:48 +0000

    Create index.testimonials-section.php

---
+++ components/index.testimonials-section.php
@@ -0,0 +1,78 @@
+<section class="testimonial-section rev-10-testimonial-section rev-12-testimonial pb-80 overflow-hidden">
+        <div class="container">
+            <div class="rev-10-testimonial-slider">
+                <div class="rev-10-single-testimony rev-12-single-testimony">
+                    <div class="rev-10-single-testimony-user">
+                        <div class="rev-10-single-testimony-user__img">
+                            <img src="assets/images/user-2.png" alt="Person">
+                        </div>
+
+                        <h6 class="rev-10-single-testimony-user__name">Robert Bruze</h6>
+                        <span class="rev-10-single-testimony-user__label">Product Designer</span>
+                    </div>
+
+                    <p class="rev-10-single-testimony__txt">
+                        All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew
+                    </p>
+                </div>
+
+                <div class="rev-10-single-testimony rev-12-single-testimony">
+                    <div class="rev-10-single-testimony-user">
+                        <div class="rev-10-single-testimony-user__img">
+                            <img src="assets/images/user-3.png" alt="Person">
+                        </div>
+
+                        <h6 class="rev-10-single-testimony-user__name">Robert Bruze</h6>
+                        <span class="rev-10-single-testimony-user__label">Product Designer</span>
+                    </div>
+
+                    <p class="rev-10-single-testimony__txt">
+                        All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew
+                    </p>
+                </div>
+
+                <div class="rev-10-single-testimony rev-12-single-testimony">
+                    <div class="rev-10-single-testimony-user">
+                        <div class="rev-10-single-testimony-user__img">
+                            <img src="assets/images/user-4.png" alt="Person">
+                        </div>
+
+                        <h6 class="rev-10-single-testimony-user__name">Robert Bruze</h6>
+                        <span class="rev-10-single-testimony-user__label">Product Designer</span>
+                    </div>
+
+                    <p class="rev-10-single-testimony__txt">
+                        All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew
+                    </p>
+                </div>
+
+                <div class="rev-10-single-testimony rev-12-single-testimony">
+                    <div class="rev-10-single-testimony-user">
+                        <div class="rev-10-single-testimony-user__img">
+                            <img src="assets/images/user-5.png" alt="Person">
+                        </div>
+
+                        <h6 class="rev-10-single-testimony-user__name">Robert Bruze</h6>
+                        <span class="rev-10-single-testimony-user__label">Product Designer</span>
+                    </div>
+
+                    <p class="rev-10-single-testimony__txt">
+                        All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew
+                    </p>
+                </div>
+
+                <div class="rev-10-single-testimony rev-12-single-testimony">
+                    <div class="rev-10-single-testimony-user">
+                        <div class="rev-10-single-testimony-user__img">
+                            <img src="assets/images/user-3.png" alt="Person">
+                        </div>
+
+                        <h6 class="rev-10-single-testimony-user__name">Robert Bruze</h6>
+                        <span class="rev-10-single-testimony-user__label">Product Designer</span>
+                    </div>
+
+                    <p class="rev-10-single-testimony__txt">All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew</p>
+                </div>
+            </div>
+        </div>
+    </section>
