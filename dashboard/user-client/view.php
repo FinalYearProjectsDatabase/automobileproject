d8e7c521d1b99add06472da8eb6eb93187aeab35
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-06-01 21:02:05 +0000

    Create view.php

---
+++ dashboard/user-client/view.php
@@ -0,0 +1,95 @@
+<?php require('../../models/session.php') ?>
+<?php require('../../config/index-settings.php');?>
+
+<?php include ('../components/head.php'); ?>
+
+<!-- header start -->
+<?php include ('../components/header.php'); ?>
+<!-- header end -->
+
+<!-- main sidebar start -->
+<?php include ('../components/menu.php'); ?>
+<!-- main sidebar end -->
+
+<!-- main content start -->
+<div class="main-content">
+    <div class="dashboard-breadcrumb mb-30">
+        <h2>Clients</h2>
+    </div>
+    <div class="row">
+        <div class="alert" id="alert-notice"></div>
+        <div class="col-xxl-4">
+            <div class="panel">
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" Method="post" enctype="multipart/form-data" id="view-user-client">
+                    <div class="panel-header">
+                        <h4>View Client</h4>
+                    </div>
+                    <div class="panel-body">
+                        <div class="mb-3">
+                            <label class="form-label" for="formGroupExampleInput">Name</label>
+                            <input type="text" name="user_client_name" class="form-control" id="formGroupExampleInput">
+                            <input type="hidden" name="user_client_id">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Date of Birth</label>
+                            <input type="date" name="user_client_dob" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Address</label>
+                            <input type="text" name="user_client_address" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Username</label>
+                            <input type="text" name="user_client_username" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Email</label>
+                            <input type="email" name="user_client_email" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Contact</label>
+                            <input type="text" name="user_client_contact" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        <button type="submit" class="btn btn-danger">Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-xxl-8">
+            <div class="panel">
+                <div class="panel-header">
+                    <h4>TableView</h4>
+                </div>
+                <div class="panel-body">
+                    <table class="table table-dashed recent-order-table" id="clientsTable">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Contact</th>
+                                <th>Registration</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- footer start -->
+<?php include ('../components/version.php') ?>
+<!-- footer end -->
+</div>
+<!-- main content end -->
+
+<?php include ('../components/footer.php') ?>
+
+<script src="../../models/admin/user-client/js/user-client-script.js"></script>
+<script src="../../models/admin/user-client/js/datatables-script.js"></script>
