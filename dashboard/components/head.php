51873f724f25e54b07a114590f9b4c6d5da77c77
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-06-01 21:01:53 +0000

    Update head.php

--- dashboard/components/head.php
+++ dashboard/components/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard | <?php echo basename('index-settings.php').PROJECT_NAME;?></title>
+    <title id="page-title">Dashboard | <?php echo basename('index-settings.php').PROJECT_NAME;?></title>
     
     <link rel="shortcut icon" href="favicon.png">
     <link rel="stylesheet" href="assets/vendor/css/all.min.css">
