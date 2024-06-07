0fe6ed1dbf35e3b7b25ed578dafb30e2186d1cf1
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-06-07 18:26:52 +0000

    Update index-blog.php

--- components/index-blog.php
+++ components/index-blog.php
@@ -1,85 +1,42 @@
 <section class="blog-section rev-10-blog-section rev-14-blog">
         <div class="container">
             <div class="rev-14-section-heading">
-                <h2 class="rev-14-section-title">Latest Blog Post</h2>
+                <h2 class="rev-14-section-title">Blog Posts</h2>
             </div>
             <div class="row gy-4 justify-content-center">
-                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
-                    <div class="rev-10-single-blog">
-                        <div class="rev-10-single-blog__img">
-                            <img src="assets/images/rev-10-blog-1.png" alt="Blog Image">
-                        </div>
+                <?php
+                    require 'config/db-parameters.php';
+                    require 'controllers/DataBaseClass.php';
+                    require 'controllers/PostsClass.php';
+                    $PostObj = new PostsClass;
 
-                        <div class="rev-10-single-blog__txt">
-                            <div class="rev-10-single-blog__author-and-date">
-                                <span class="blog-author">James Hames</span>
-                                <span class="blog-date">22 Oct 2021</span>
+                    $getPosts = $PostObj->get_all_posts();
+
+                    $response = json_decode($getPosts);
+
+                    foreach($response as $value){
+                        
+                ?>
+                    <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
+                        <div class="rev-10-single-blog">
+                            <div class="rev-10-single-blog__img">
+                                <img src="../post-images/<?php echo $value->post_featured_image?>" alt="Blog Image">
                             </div>
-                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
-                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
-                                mos sodales suscipit tortor ditaemcos condimentum
-                                lacus meleifend.Quisquemos sodales suscipit.</p>
-                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
+
+                            <div class="rev-10-single-blog__txt">
+                                <div class="rev-10-single-blog__author-and-date">
+                                    <span class="blog-date"><?php echo date("l F j, Y", strtotime($value->created_at)) ?></span>
+                                </div>
+                                <h3 class="rev-10-single-blog__title"><a href="blog-detail.php?blog=<?php echo $value->post_id?>"><?php echo $value->post_title?> </a></h3>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
-                    <div class="rev-10-single-blog">
-                        <div class="rev-10-single-blog__img">
-                            <img src="assets/images/rev-10-blog-2.png" alt="Blog Image">
-                        </div>
 
-                        <div class="rev-10-single-blog__txt">
-                            <div class="rev-10-single-blog__author-and-date">
-                                <span class="blog-author">James Hames</span>
-                                <span class="blog-date">22 Oct 2021</span>
-                            </div>
-                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
-                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
-                                mos sodales suscipit tortor ditaemcos condimentum
-                                lacus meleifend.Quisquemos sodales suscipit.</p>
-                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
-                    <div class="rev-10-single-blog">
-                        <div class="rev-10-single-blog__img">
-                            <img src="assets/images/rev-10-blog-3.png" alt="Blog Image">
-                        </div>
+                <?php
 
-                        <div class="rev-10-single-blog__txt">
-                            <div class="rev-10-single-blog__author-and-date">
-                                <span class="blog-author">James Hames</span>
-                                <span class="blog-date">22 Oct 2021</span>
-                            </div>
-                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
-                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
-                                mos sodales suscipit tortor ditaemcos condimentum
-                                lacus meleifend.Quisquemos sodales suscipit.</p>
-                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-sm-6 col-9 col-xs-12">
-                    <div class="rev-10-single-blog">
-                        <div class="rev-10-single-blog__img">
-                            <img src="assets/images/rev-10-blog-4.png" alt="Blog Image">
-                        </div>
+                    }
 
-                        <div class="rev-10-single-blog__txt">
-                            <div class="rev-10-single-blog__author-and-date">
-                                <span class="blog-author">James Hames</span>
-                                <span class="blog-date">22 Oct 2021</span>
-                            </div>
-                            <h3 class="rev-10-single-blog__title"><a href="blog-details.html">Take Care Your Child and Play with Them, Know the Best way For Caring Kids. </a></h3>
-                            <p class="rev-10-single-blog__description">lacinia an, tincidunt risus ac, consequat velit. Quisque
-                                mos sodales suscipit tortor ditaemcos condimentum
-                                lacus meleifend.Quisquemos sodales suscipit.</p>
-                            <a href="#" class="def-btn rev-10-def-btn">read more</a>
-                        </div>
-                    </div>
-                </div>
+                ?>
             </div>
         </div>
     </section>
