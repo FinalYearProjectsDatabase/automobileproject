fa696d7fd0f9b29e944489cd7cfb516b3e06e03a
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-06-08 06:03:34 +0000

    Update edit-service.php

--- models/admin/service/server/edit-service.php
+++ models/admin/service/server/edit-service.php
@@ -2,27 +2,27 @@
 
     session_start();
 
-    if(isset($_GET['product_id'])){
+    if(isset($_GET['service_id'])){
         // this file helps us to get the uuid method and username method
         require('../../../../config/functions.php');
         // settings
         require('../../../../config/index-settings.php');
         // database parameters
         require '../../../../config/db-parameters.php';
-        $product_id = filter_input(INPUT_GET, "product_id", FILTER_SANITIZE_SPECIAL_CHARS);
+        $service_id = filter_input(INPUT_GET, "service_id", FILTER_SANITIZE_SPECIAL_CHARS);
         // database class
         require('../../../../controllers/DataBaseClass.php');
-        // products class
-        require('../../../../controllers/ProductsClass.php');
+        // services class
+        require('../../../../controllers/ServicesClass.php');
 
-        // initialize the product class with an object
-        $productObj = new ProductsClass;
+        // initialize the service class with an object
+        $serviceObj = new ServicesClass;
 
-        // get the new product method and we pass the various parameters to create a new clients
-        $productData = $productObj->get_product($product_id);
+        // get the new service method and we pass the various parameters to create a new clients
+        $serviceData = $serviceObj->get_service($service_id);
 
         // get the outcome of the method which is to return a json response and convert it in array
-        $outcome = json_decode($productData);
+        $outcome = json_decode($serviceData);
 
         echo json_encode($outcome);
     }
